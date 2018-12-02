b39872762295d259e6c46e14657f3908ad7f5d4b
jrshutske <jackshutske@gmail.com>
2018-12-02 04:43:21 -0600

    added about static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
 
   get '/welcome', to: 'welcome#index'
   get '/hello', to: 'application#hello'
