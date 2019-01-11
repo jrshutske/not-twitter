917336c715890921de520e876eac4100a20f19b3
jrshutske <jackshutske@gmail.com>
2019-01-11 11:13:06 -0600

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-
   resources :microposts
   resources :users
   resources :widgets
-
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-
-  get '/welcome', to: 'welcome#index'
-  get '/hello', to: 'application#hello'
-  get '/goodbye', to: 'application#goodbye'
-
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/help', to: 'static_pages#help'
+  get '/signup', to: 'users#new'
 end
