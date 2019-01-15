0181b649b22e877d295746bd333b8c19d769e2e1
jrshutske <jackshutske@gmail.com>
2019-01-15 15:00:34 -0600

    implement basic login with logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :microposts
   resources :users
   resources :widgets
-  root 'static_pages#home'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/help', to: 'static_pages#help'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  root    'static_pages#home'
+  get     '/about',   to: 'static_pages#about'
+  get     '/contact', to: 'static_pages#contact'
+  get     '/help',    to: 'static_pages#help'
+  get     '/signup',  to: 'users#new'
+  post    '/signup',  to: 'users#create'
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
+
 end
