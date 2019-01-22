e9432ae430f2d462b1575cfd39bcdb4c39259071
jrshutske <jackshutske@gmail.com>
2019-01-22 12:18:30 -0600

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   resources :widgets
+  resources :account_activations, only: [:edit]
   root    'static_pages#home'
   get     '/about',   to: 'static_pages#about'
   get     '/contact', to: 'static_pages#contact'
